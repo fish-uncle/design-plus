6acdfaf44eae4d0e836036e642d4c9eeca92eb31
zhepei.shen <zhepei.shen@eslink.com>
2021-12-09 17:28:04 +0800

    fix: 🐞 Make deploy.sh executable

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env sh
-
 set -e
-
 npm run docs:build
-
 cd docs/.vitepress/dist
-
 git init
 git add -A
 git commit -m 'deploy'
-
 git push -f https://${access_token}@github.com/fish-uncle/design-plus-doc.git master:gh-pages
-
 cd -
